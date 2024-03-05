2cd3e556daae1cbffedc905e7a14043d4914dee5
Jens Herold <j.herold@ipe-evaluation.de>
2024-03-05 17:40:05 +0100

    ...

--- scripts-dist/use-current-role.js
+++
@@ -1,6 +0,0 @@
-import { useSession } from "next-auth/react";
-export var useCurrentRole = function () {
-    var _a, _b;
-    var session = useSession();
-    return (_b = (_a = session.data) === null || _a === void 0 ? void 0 : _a.user) === null || _b === void 0 ? void 0 : _b.role;
-};
