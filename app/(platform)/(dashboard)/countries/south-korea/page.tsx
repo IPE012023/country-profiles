8c55514cf373c285a1521bfa63bc48dd78973801
Jens Herold <j.herold@ipe-evaluation.de>
2024-03-06 12:05:39 +0100

    ...

--- app/(platform)/(dashboard)/countries/south-korea/page.tsx
+++ app/(platform)/(dashboard)/countries/south-korea/page.tsx
@@ -5,36 +5,6 @@ import React, { useState } from "react";
 const CountryProfilePage = () => {
   const countryName = "Tax Indexation in the Repbublic of Korea"; // You can make this dynamic
   const message = "Key Highlights of the Project"; // Example message
-
-  // State to track upload status
-  const [uploadStatus, setUploadStatus] = useState('');
-
-  const handleUploadData = async () => {
-    setUploadStatus('Uploading...');
-
-    try {
-      const response = await fetch('/api/uploadData', {
-        method: 'POST',
-      });
-
-      if (response.ok) {
-        setUploadStatus('Upload successful!');
-      } else {
-        const errorText = await response.text();
-        setUploadStatus(`Upload failed: ${errorText}`);
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Failed to upload data:', error.message);
-        setUploadStatus(`Upload failed: ${error.message}`);
-      } else {
-        // If it's not an Error instance, handle it accordingly
-        console.error('Failed to upload data:', error);
-        setUploadStatus('Upload failed: An unexpected error occurred.');    
-    }
-  };
-
-  }
   
   return (
     <div className="flex flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-400 to-slate-800 w-full min-h-screen">
@@ -43,17 +13,6 @@ const CountryProfilePage = () => {
         <h1 className="text-4xl font-bold text-white">{countryName}</h1>
         <p className="text-xl text-slate-200 mt-2">{message}</p>
       </div>
-
-       {/* Upload Button */}
-       <button
-        onClick={handleUploadData}
-        className="mb-5 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-300"
-      >
-        Start Upload
-      </button>
-
-      {/* Feedback Message */}
-      {uploadStatus && <p className="text-white">{uploadStatus}</p>}
 
       {/* Key Messages Box */}
       <div className="w-full p-10 bg-slate-700 text-white">
